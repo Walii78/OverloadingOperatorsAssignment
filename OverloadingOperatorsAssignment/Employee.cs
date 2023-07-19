62d47a766881daa8f5a9f661635dc2a9095178d4
Walii78 <129457570+Walii78@users.noreply.github.com>
2023-07-18 18:26:50 -0700

    Add project files.

---
+++ OverloadingOperatorsAssignment/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperatorsAssignment
+{
+    //Create the Employee class with Id, FirstName, and LastName properties
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overload the "==" operator to compare Employee objects by their Id property
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+            if (emp1 is null || emp2 is null)
+                return false;
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overload the "!=" operator as well, following the requirement of operator overloading
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
---
+++ OverloadingOperatorsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate two Employee objects and assign values to their properties
+            Employee employee1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee employee2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" };
+
+            // Compare the two Employee objects using the overloaded "==" and "!=" operators
+            if (employee1 == employee2)
+                Console.WriteLine("Employee 1 and Employee 2 are equal.");
+            else
+                Console.WriteLine("Employee 1 and Employee 2 are not equal.");
+
+            Console.ReadLine();
+        }
+    }
+}
